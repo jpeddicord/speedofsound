6a82079f0eab8d4f6fb0702396ef194b4edede73
Jacob Peddicord <jacob@peddicord.net>
2012-04-24 09:55:26 -0400

    Initial commit.

---
+++ src/edu/osu/musicspeed/settings.java
@@ -0,0 +1,15 @@
+package edu.osu.musicspeed;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class settings extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
